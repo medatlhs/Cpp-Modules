9ea995ec8d8f14c8d2265e23b476731029ec315b
Mohamed Ait-lahssen <atlhs1337@gmail.com>
2025-02-21 00:00:28 +0100

    push

---
+++ Module 02/ex03/src/bsp.cpp
@@ -0,0 +1,6 @@
+#include "../Includes.hpp"
+
+bool bsp( Point const a, Point const b, Point const c, Point const point) {
+        //A = [ x1(y2 – y3) + x2(y3 – y1) + x3(y1-y2)]/2 
+    float A = a.x(b.y - )
+}
