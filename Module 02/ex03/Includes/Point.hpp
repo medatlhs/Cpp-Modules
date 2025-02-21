dfc76dba7928cbd6c7534651bcb1fd1ea619f176
Mohamed Ait-lahssen <atlhs1337@gmail.com>
2025-02-21 20:01:27 +0100

    added Module 02

--- Module 02/ex03/Includes/Point.hpp
+++ Module 02/ex03/Includes/Point.hpp
@@ -1,15 +1,25 @@
-#include <iostream>
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include "./Fixed.hpp"
 
 class Point
 {
 private:
-    float x;
-    float y;
+    Fixed x;
+    Fixed y;
+
 public:
     Point() : x(0), y(0) {};
     Point(const float x, const float y);
     Point(const Point &other);
     Point& operator =(const Point &other);
+    float getX() const;
+    float getY() const;
+
     ~Point();
 };
 
+bool bsp( Point const a, Point const b, Point const c, Point const point);
+
+#endif
--- Module 02/ex03/src/main.cpp
+++ Module 02/ex03/src/main.cpp
@@ -1,8 +1,14 @@
-#include "../includes/Fixed.hpp"
-#include "../includes/Point.hpp"
+#include "../Includes/Fixed.hpp"
+#include "../Includes/Point.hpp"
 
-int main(int argc, char const *argv[])
+
+int main()
 {
-    
+    Point a(1, 1);
+    Point b(6, 1);
+    Point c(3, 4);
+    Point point(5, 1.99999);
+
+    bsp(a, b, c, point) ? std::cout << "true\n" : std::cout << "false\n";
     return 0;
 }
