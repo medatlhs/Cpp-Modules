cd4004cf2f1b4e3f9cbc1a020465878d4213659e
Mohamed Ait-lahssen <atlhs1337@gmail.com>
2025-02-24 05:36:24 +0000

    feat: implement inheritance and base/derived class hierarchy

---
+++ Module 03/ex00/source/main.cpp
@@ -0,0 +1,10 @@
+#include "../Includes/ClapTrap.hpp"
+
+int main() {
+    ClapTrap charachter("lmlsdn");
+    charachter.attack("lmhchach");
+    charachter.takeDamage(8);
+    charachter.attack("lmhchach");
+    charachter.beRepaired(1);
+    return 0;
+}
