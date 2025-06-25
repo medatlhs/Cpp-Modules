13ffbe644262d063379932f051e245fef7a19e9f
Mohamed Ait-lahssen <atlhs1337@gmail.com>
2025-06-25 16:48:16 +0100

    LLLLLLLLLMMMMMMMMMMMMMMMMMMLLLLLLLLLLLSSSSSSSSSSDDDDDDDDDDNNNNNNNNNN

---
+++ Module 05/ex01/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <exception>
+#include <iostream>
+
+class Bureaucrat
+{
+    private:
+        const std::string _name;
+        int _grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string name, int grade);
+        Bureaucrat(const Bureaucrat& copy);
+        Bureaucrat& operator= (const Bureaucrat& other);
+        ~Bureaucrat();
+
+        class GradeTooHighException: public std::exception {
+            public:
+               const char *what() const throw();
+        };
+
+        class GradeTooLowException: public std::exception {
+            public:
+                const char *what() const throw();
+        };
+
+        std::string getName() const;
+        int         getGrade() const;
+
+        void increment();
+        void decrement();
+};
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat &obj);
+
+#endif
+
+/*
+Because GradeTooHighException by itself is just a type, not an object.
+
+*/
---
+++ Module 05/ex01/Form.hpp
@@ -0,0 +1,43 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include "Bureaucrat.hpp"
+#include <string>
+#include <iostream>
+#include <exception>
+
+class Form
+{
+    private:
+        const std::string _name;
+        bool _isSigned;
+        const int _signGrade;
+        const int _execGrade;
+
+    public:
+        Form();
+        Form(std::string name, int signGrade, int execGrade);
+        Form(const Form &copy);
+        Form& operator= (const Form &other);
+        ~Form();
+        
+        void besigned(Bureaucrat &b);
+        
+        std::string get_Name() const;
+        int get_SignGrade() const;
+        int get_ExecGrade() const;
+        bool check_Ifsigned() const;
+
+
+        class GradeTooHighException: public std::exception {
+            public:
+                const char* what() const throw();
+        };
+
+        class GradeTooLowException: public std::exception {
+            public:
+                const char* what() const throw();
+        };
+};
+
+#endif
