a2f697b2ce51e77b5e89fc44a35e6151792ffbc0
Mohamed Ait-lahssen <atlhs1337@gmail.com>
2025-06-29 21:13:18 +0100

    finished ex03

--- Module 05/ex03/Includes/Intern.hpp
+++ Module 05/ex03/Includes/Intern.hpp
@@ -1,19 +1,22 @@
 #ifndef INTERN_HPP
 #define INTERN_HPP
 
+#include "PresidentialPardonForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include "AForm.hpp"
 #include <string>
 
 class Intern {
 
-public:
-    Intern();
-    Intern(const std::string &target);
-    Intern(const Intern &other);
-    Intern &operator=(const Intern &other);
-    ~Intern();
-    
-    AForm* makeForm(std::string formName, std::string formTarget);
+    public:
+        Intern();
+        Intern(const std::string &target);
+        Intern(const Intern &other);
+        Intern &operator=(const Intern &other);
+        ~Intern();
+
+        AForm* makeForm(const std::string &form_name, const std::string &form_target);
 };
 
 #endif
